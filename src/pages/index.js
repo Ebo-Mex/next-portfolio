27158e7e9fc6d0b91080032f34cf32a2339f48ec
Emma_Bolanos <emmanuel.arturo.42@gmail.com>
2023-04-22 00:12:51 -0600

    chore: update favicon location and add title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,32 @@
 import MainView from "@/pages/MainView";
+import Head from "next/head";
 
 export default function Home() {
     return (
         <>
+            <Head>
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicon-16x16.png"
+                />
+                <link rel="manifest" href="/site.webmanifest" />
+                <meta name="msapplication-TileColor" content="#ffae23" />
+                <meta name="theme-color" content="#ffae23" />
+                <title>Emmanuel - Frontend dev</title>
+            </Head>
             <MainView />
         </>
     );
