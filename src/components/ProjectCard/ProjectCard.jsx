3b4ca9278800e2fcf3571e0148863923764d90f0
Emma_Bolanos <58895233+Ebo-Mex@users.noreply.github.com>
2023-04-21 22:30:00 -0600

    Feature/projects section (#1)
    * feat: projects section progress
    
    * feat: add mock data and improve project section styles

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,9 +1,21 @@
-export const ProjectCard = ({ label, description, thumbnailImageSrc }) => {
+import Image from "next/image";
+
+export const ProjectCard = ({ label, description, thumbnailImageSrc, idx }) => {
+    const isOdd = idx % 2 === 1;
     return (
-        <div>
-            {thumbnailImageSrc ? <img src={thumbnailImageSrc} alt="" /> : null}
-            <h3>{label}</h3>
-            <p>{description}</p>
+        <div className={"projectCardContainer " + (isOdd ? "odd" : "even")}>
+            {thumbnailImageSrc ? (
+                <Image
+                    width={200}
+                    height={200}
+                    src={thumbnailImageSrc}
+                    alt=""
+                />
+            ) : null}
+            <div className="cardData">
+                <h3 className="cardTitle">{label}</h3>
+                <p className="cardDescription">{description}</p>
+            </div>
         </div>
     );
 };
--- src/pages/ProjectsDisplay/ProjectsDisplay.jsx
+++ src/pages/ProjectsDisplay/ProjectsDisplay.jsx
@@ -2,16 +2,17 @@ import { ProjectCard } from "@/components";
 
 export const ProjectsDisplay = ({ projects }) => {
     return (
-        <div>
+        <div className="projectsDisplayContainer">
             <h2>Projects</h2>
-            <ul>
+            <ul className="projectsList">
                 {projects.map(
-                    ({ label, description, thumbnailImageSrc, id }) => (
+                    ({ label, description, thumbnailImageSrc, id }, idx) => (
                         <li key={id}>
                             <ProjectCard
                                 label={label}
                                 description={description}
                                 thumbnailImageSrc={thumbnailImageSrc}
+                                idx={idx}
                             />
                         </li>
                     )
