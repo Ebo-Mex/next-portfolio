6b2033774276430c20506188c9d9260d5d53fac4
Emma_Bolanos <58895233+Ebo-Mex@users.noreply.github.com>
2023-04-23 00:06:09 -0600

    chore: reorganize site text content (#5)

--- src/pages/MainView/MainView.jsx
+++ src/pages/MainView/MainView.jsx
@@ -4,76 +4,35 @@ import ProjectsDisplay from "@/pages/ProjectsDisplay";
 import BragSection from "@/pages/BragSection";
 import MoreAboutMe from "@/pages/MoreAboutMe";
 import { NavBar } from "@/components";
+import { useState, useEffect } from "react";
 
 export const MainView = () => {
-    const projects = [
-        {
-            label: "Autonomous Drone Competition",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            thumbnailImageSrc: "https://picsum.photos/200/300",
-            id: 0,
-        },
-        {
-            label: "Object Recognition and Tracking Research",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            thumbnailImageSrc: "https://picsum.photos/200/300",
-            id: 1,
-        },
-        {
-            label: "Autonomous Line Follower Car",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            thumbnailImageSrc: "https://picsum.photos/200/300",
-            id: 2,
-        },
-        {
-            label: "Arcade Game Prototype",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            thumbnailImageSrc: "https://picsum.photos/200/300",
-            id: 3,
-        },
-    ];
-    const achievements = [
-        {
-            title: "Something something",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            reference: {
-                linkText: "link",
-                url: "https://google.com",
-            },
-        },
-        {
-            title: "Another something",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            reference: {
-                linkText: "link",
-                url: "https://google.com",
-            },
-        },
-        {
-            title: "some   thing",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            reference: {
-                linkText: "link",
-                url: "https://google.com",
-            },
-        },
-    ];
+    const [portfolioData, setPortfolioData] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch("assets/data/portfolioContent.json")
+            .then((res) => res.json())
+            .then((data) => {
+                setPortfolioData(data);
+                setLoading(false);
+            });
+    }, []);
 
     return (
         <>
-            <NavBar />
-            <Welcome />
-            <Toolbox />
-            <ProjectsDisplay projects={projects} />
-            <BragSection achievements={achievements} />
-            <MoreAboutMe />
+            {loading ? (
+                <div>loading</div>
+            ) : (
+                <div>
+                    <NavBar />
+                    <Welcome introduction={portfolioData.introduction} />
+                    <Toolbox items={portfolioData.toolboxItems} />
+                    <ProjectsDisplay projects={portfolioData.projects} />
+                    <BragSection achievements={portfolioData.achievements} />
+                    <MoreAboutMe />
+                </div>
+            )}
         </>
     );
 };
