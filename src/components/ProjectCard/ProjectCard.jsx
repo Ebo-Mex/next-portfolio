7ad07dd6b3536933d88ea05972c4cb58722f2f3d
Emma_Bolanos <emmanuel.arturo.42@gmail.com>
2023-04-23 17:58:23 -0600

    chore: progress on expand logic, add more data and assets

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,20 +1,40 @@
 import Image from "next/image";
+import { useState } from "react";
 
-export const ProjectCard = ({ label, description, thumbnailImageSrc, idx }) => {
+export const ProjectCard = ({ label, content, thumbnailImageSrc, idx }) => {
+    const [isExpanded, setIsExpanded] = useState(false);
+
     const isOdd = idx % 2 === 1;
+
     return (
         <div className={"projectCardContainer " + (isOdd ? "odd" : "even")}>
             {thumbnailImageSrc ? (
-                <Image
-                    width={200}
-                    height={200}
-                    src={thumbnailImageSrc}
-                    alt=""
-                />
+                <div className="imageContainer">
+                    <Image
+                        className="image"
+                        fill
+                        sizes="30vw"
+                        object-fit="cover"
+                        src={thumbnailImageSrc}
+                        alt=""
+                    />
+                </div>
             ) : null}
             <div className="cardData">
                 <h3 className="cardTitle">{label}</h3>
-                <p className="cardDescription">{description}</p>
+                <div>
+                    <p className="cardDescription">{content[0]}</p>
+                    {isExpanded
+                        ? content
+                              .slice(1)
+                              .map((paragraph, pidx) => (
+                                  <p key={`${idx}-${pidx}`}>{paragraph}</p>
+                              ))
+                        : ""}
+                </div>
+                <button onClick={() => setIsExpanded(!isExpanded)}>
+                    {isExpanded ? "Shrink" : "Expand"}
+                </button>
             </div>
         </div>
     );
