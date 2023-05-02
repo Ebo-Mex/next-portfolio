0fbf5689ef2ddc48ca16ccb49f94a7c9967032fb
Emma_Bolanos <emmanuel.arturo.42@gmail.com>
2023-05-01 20:58:47 -0600

    chore: update achievement cards

--- src/components/AchievementCard/AchievementCard.jsx
+++ src/components/AchievementCard/AchievementCard.jsx
@@ -1,7 +1,13 @@
-export const AchievementCard = ({ title, description, reference }) => {
+export const AchievementCard = ({
+    title,
+    organization,
+    description,
+    reference,
+}) => {
     return (
         <div className="achievementCardContainer">
             <h3 className="title">{title}</h3>
+            <p className="organization">Awarded by: {organization}</p>
             <p className="description">{description}</p>
             <p className="reference">
                 For more information, please visit:{" "}
