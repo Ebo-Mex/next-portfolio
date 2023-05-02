8d9dec3e0f9a31b7db6d23ed47533009e0b2b0c1
Emma_Bolanos <58895233+Ebo-Mex@users.noreply.github.com>
2023-05-01 20:59:10 -0600

    chore: update achievement cards (#9)

--- src/pages/BragSection/BragSection.jsx
+++ src/pages/BragSection/BragSection.jsx
@@ -5,15 +5,18 @@ export const BragSection = ({ achievements }) => {
         <div className="bragSectionContainer">
             <h2>Achivements and Rewards</h2>
             <ul className="achievementsList">
-                {achievements.map(({ title, description, reference }, idx) => (
-                    <li key={idx}>
-                        <AchievementCard
-                            title={title}
-                            description={description}
-                            reference={reference}
-                        />
-                    </li>
-                ))}
+                {achievements.map(
+                    ({ title, organization, description, reference }, idx) => (
+                        <li key={idx}>
+                            <AchievementCard
+                                title={title}
+                                organization={organization}
+                                description={description}
+                                reference={reference}
+                            />
+                        </li>
+                    )
+                )}
             </ul>
         </div>
     );
